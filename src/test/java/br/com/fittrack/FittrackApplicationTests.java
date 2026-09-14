f013dfb23d15cbc00ec2b542705c6b3fc402a6c5
eromao <eromao@meplis.com>
2026-09-14 15:56:59 -0300

    first commit

---
+++ src/test/java/br/com/fittrack/FittrackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fittrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
